61148963ae032605908e496ea635310c2c90305a
ph-souzaa <souza.pharaujo@gmail.com>
2023-01-08 21:37:51 -0300

    criacao de usarios

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { categoriesRouter } from "./categories.routes";
 import { specificationsRouter } from "./specifications.routes";
+import { usersRouter } from "./users.routes";
 
 const router = Router();
 
 router.use("/categories", categoriesRouter);
 router.use("/specifications", specificationsRouter);
+router.use("/users", usersRouter);
 
 export { router };
--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -4,6 +4,8 @@ import { CategoriesRepository } from '../../modules/cars/repositories/implementa
 
 import { ISpecificationsRepository } from '../../modules/cars/repositories/ISpecificationsRepository';
 import { SpecificationsRepository } from '../../modules/cars/repositories/implementations/SpecificationsRepository';
+import { IUsersRepository } from '../../modules/accounts/repositories/IUsersRepository';
+import { UsersRepository } from '../../modules/accounts/repositories/implementations/UsersRepository';
 
 
 container.registerSingleton<ICategoriesRepository>(
@@ -13,4 +15,9 @@ container.registerSingleton<ICategoriesRepository>(
 container.registerSingleton<ISpecificationsRepository>(
   "SpecificationsRepository",
   SpecificationsRepository
+)
+
+container.registerSingleton<IUsersRepository>(
+  "UsersRepository",
+  UsersRepository
 )
